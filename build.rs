7a1172ed7ea6b81eb3dcca68ca4c05797756de5e
C4rbn <mokshanbasuru@gmail.com>
2026-04-29 19:13:44 +0530

    Update build script for BPF compilation

--- build.rs
+++ build.rs
@@ -3,15 +3,21 @@ use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
+    println!("cargo:rerun-if-changed=src/xdp-interceptor/interceptor.bpf.c");
+    println!("cargo:rerun-if-changed=build.rs");
+
     let out_dir = env::var("OUT_DIR").unwrap();
     let out_path = PathBuf::from(out_dir).join("interceptor.o");
-    println!("cargo:rerun-if-changed=src/xdp-interceptor/interceptor.bpf.c");
+
     let status = Command::new("clang")
         .args([
             "-O2",
-            "-target", "bpf",
-            "-mcpu=v2",
             "-g",
+            "-target", "bpfel",
+            "-mcpu=v2",
+            "-fno-addrsig",
+            "-fno-asynchronous-unwind-tables",
+            "-fno-ident",
             "-D__TARGET_ARCH_x86",
             "-I/usr/include",
             "-I/usr/include/x86_64-linux-gnu",
@@ -20,6 +26,7 @@ fn main() {
         ])
         .status()
         .expect("BPF Core Failure");
+
     if !status.success() {
         panic!("Clang BPF compilation failed");
     }
