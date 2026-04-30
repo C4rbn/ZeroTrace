eea543ccd2bf563cd1351f77bea23965b96fac99
C4rbn <mokshanbasuru@gmail.com>
2026-04-30 21:05:55 +0530

    Refactor main function to send different packets

--- src/key.c
+++ src/key.c
@@ -4,25 +4,11 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-// Usage: ./key <target_ip> <port>
-// Ports: 1337 (Authorize), 65535 (Self-Destruct)
-
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        printf("Usage: %s <ip> <port>\n", argv[0]);
-        return 1;
-    }
-
-    int sock = socket(AF_INET, SOCK_DGRAM, 0);
-    struct sockaddr_in target;
-    target.sin_family = AF_INET;
-    target.sin_port = htons(atoi(argv[2]));
-    target.sin_addr.s_addr = inet_addr(argv[1]);
-
-    const char *payload = "\xde\xad\xbe\xef"; // Dummy data
-    sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&target, sizeof(target));
-
-    printf("[+] Packet sent to %s:%s\n", argv[1], argv[2]);
-    close(sock);
+    if (argc < 4) return 1;
+    int s = socket(AF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in t = { .sin_family = AF_INET, .sin_port = htons(atoi(argv[3])), .sin_addr.s_addr = inet_addr(argv[1]) };
+    sendto(s, "\x01", 1, 0, (struct sockaddr *)&t, sizeof(t));
+    close(s);
     return 0;
 }
