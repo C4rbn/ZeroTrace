c07a7205d38238aa2476b023ebe536fe115dab45
C4rbn <mokshanbasuru@gmail.com>
2026-04-30 06:58:42 +0530

    Replace BPF compilation with SkeletonBuilder usage

--- build.rs
+++ build.rs
@@ -1,33 +1,12 @@
+use libbpf_cargo::SkeletonBuilder;
 use std::env;
 use std::path::PathBuf;
-use std::process::Command;
 
 fn main() {
-    println!("cargo:rerun-if-changed=src/xdp-interceptor/interceptor.bpf.c");
-    println!("cargo:rerun-if-changed=build.rs");
-
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let out_path = PathBuf::from(out_dir).join("interceptor.o");
-
-    let status = Command::new("clang")
-        .args([
-            "-O2",
-            "-g",
-            "-target", "bpfel",
-            "-mcpu=v2",
-            "-fno-addrsig",
-            "-fno-asynchronous-unwind-tables",
-            "-fno-ident",
-            "-D__TARGET_ARCH_x86",
-            "-I/usr/include",
-            "-I/usr/include/x86_64-linux-gnu",
-            "-c", "src/xdp-interceptor/interceptor.bpf.c",
-            "-o", out_path.to_str().unwrap(),
-        ])
-        .status()
-        .expect("BPF Core Failure");
-
-    if !status.success() {
-        panic!("Clang BPF compilation failed");
-    }
+    let out = PathBuf::from(env::var("OUT_DIR").unwrap()).join("zerotrace.skel.rs");
+    SkeletonBuilder::new()
+        .source("src/bpf/zerotrace.bpf.c")
+        .debug(false)
+        .build_and_generate(&out)
+        .unwrap();
 }
