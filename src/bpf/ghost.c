22da053cde8b94b1ccd41c1c46b4f5cd4f91b9ea
C4rbn <mokshanbasuru@gmail.com>
2026-04-30 10:29:08 +0530

    Change key type in eth_state and update logic

--- src/bpf/ghost.c
+++ src/bpf/ghost.c
@@ -7,7 +7,7 @@ char _license[] SEC("license") = "GPL";
 struct {
     __uint(type, BPF_MAP_TYPE_LRU_HASH);
     __uint(max_entries, 1024);
-    __type(key, __u32);
+    __type(key, __u8[16]);
     __type(value, __u64);
 } eth_state SEC(".maps");
 
@@ -17,23 +17,36 @@ int virtnet_poll(struct xdp_md *ctx) {
     void *data = (void *)(long)ctx->data;
 
     __u8 *ptr = data;
-    if (ptr + 34 > (__u8 *)data_end) return XDP_PASS;
+    if (ptr + 14 > (__u8 *)data_end) return XDP_PASS;
 
-    // Fast TOTP: Use bit-shift instead of division for Verifier
-    // (now >> 35) is approx 34.3 seconds
+    __u16 h_proto = *(__u16 *)(ptr + 12);
+    __u8 addr[16] = {0};
+    __u16 pkt_id = 0;
+    __u8 is_ipv6 = 0;
+
+    if (h_proto == 0x0008) { 
+        if (ptr + 34 > (__u8 *)data_end) return XDP_PASS;
+        *(__u32 *)&addr[0] = *(__u32 *)(ptr + 26);
+        pkt_id = *(__u16 *)(ptr + 18);
+    } else if (h_proto == 0xDD86) { 
+        if (ptr + 54 > (__u8 *)data_end) return XDP_PASS;
+        for(int i=0; i<16; i++) addr[i] = *(ptr + 22 + i);
+        pkt_id = *(__u16 *)(ptr + 4); 
+        is_ipv6 = 1;
+    } else {
+        return XDP_PASS;
+    }
+
     __u64 now = bpf_ktime_get_ns();
-    __u32 secret = (__u32)((now >> 35) ^ 0xDEADBEEF);
+    __u32 secret = (__u32)((now >> 35) ^ SEED);
 
-    __u32 src_ip = *(__u32 *)(ptr + 26);
-    __u16 ip_id = *(__u16 *)(ptr + 18);
-
-    if (ip_id == (__u16)secret) {
+    if (pkt_id == (__u16)secret) {
         __u64 ts = now;
-        bpf_map_update_elem(&eth_state, &src_ip, &ts, BPF_ANY);
+        bpf_map_update_elem(&eth_state, &addr, &ts, BPF_ANY);
         return XDP_DROP;
     }
 
-    if (!bpf_map_lookup_elem(&eth_state, &src_ip)) return XDP_DROP;
+    if (!bpf_map_lookup_elem(&eth_state, &addr)) return XDP_DROP;
 
     return XDP_PASS;
 }
